5640caed60c8ec9929c022f9d8f188eedfb19857
hoangluu2001 <hoangthehoaluu2gmail.com>
2020-11-09 15:01:32 +0700

    up

--- QuadraticEquation.php
+++ QuadraticEquation.php
@@ -1,1 +1,32 @@
 <?php
+class QuadraticEquation {
+    public $a;
+    public $b;
+    public $c;
+
+    function __construct($a,$b,$c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    public function getRoot1(){
+        $delta =( $this->b * $this->b -4 *( $this->a * $this->c ));
+        if ($delta > 0) {
+         return "pt co nghiem la :" . $x1 = (-$this->b + sqrt($delta)) / 2 * $this->a  .  $x2 = (-$this->b - sqrt($delta))/2*$this->a;
+
+        }else if ($delta==0){
+            return "pt co nghiem la :" . (-$this->b + sqrt($delta)) / 2 * $this->a ;
+
+        }else{
+            return "pt ko co nghiem ";
+        }
+    }
+
+}
+$a = 1;
+$b = 5 ;
+$c = 5 ;
+$tinh = new QuadraticEquation($a,$b,$c);
+echo $tinh->getRoot1();
