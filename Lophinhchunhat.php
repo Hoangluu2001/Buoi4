259c9543a5a7177a0c96fa0b95b18fe23e3036b6
hoangluu2001 <hoangthehoaluu2gmail.com>
2020-11-09 10:54:30 +0700

    up

--- Lophinhchunhat.php
+++ Lophinhchunhat.php
@@ -1,1 +1,28 @@
 <?php
+class Rectangle
+{
+    var $width;
+    var $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getArea()
+    {
+        return $this->width * $this->height;
+    }
+    public function getPerimeter(){
+        return(($this->width + $this->height) * 2);
+    }
+    public function display(){
+        return "Rectangle{"."width=". $this->width.",height=".$this->height."}";
+    }
+}
+$height = 20 ;
+$width = 10 ;
+$chunhat = new Rectangle($width ,$height);
+//echo $chunhat->getArea();
+echo $chunhat ->getPerimeter();
